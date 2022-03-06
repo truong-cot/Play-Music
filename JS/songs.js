ebd347c470603b1f8d8dec2ecf32303991525bd9
truong-cot <engtruong1609@gmail.com>
2022-03-06 22:31:09 +0700

    update

--- JS/songs.js
+++ JS/songs.js
@@ -3,55 +3,55 @@ export const songs = [
         name: "3107 1",
         singer: "Duongg",
         path: '../MUSIC/3107 1.mp3',
-        image: '../Image/3107 1.jpg',
+        image: '../asset/Image/3107 1.jpg',
       },
       {
         name: "3107 2",
         singer: "Duongg",
         path: '../MUSIC/3107 2.mp3',
         image:
-        '../Image/3107 2.jpg'
+        '../asset/Image/3107 2.jpg'
       },
       {
         name: "3107 3",
         singer: "Duongg",
         path: '../MUSIC/3107 3.mp3',
-        image: '../Image/3107 3.jpg'
+        image: '../asset/Image/3107 3.jpg'
       },
       {
         name: "Đông kiếm em",
         singer: "Vũ ...",
         path: '../MUSIC/DongKiemEm.mp3',
-        image: '../Image/DongKiemEm.jpg'
+        image: '../asset/Image/DongKiemEm.jpg'
       },
       {
         name: "Ai chung tình được mãi",
         singer: "Đinh Tùng Huy",
         path: '../MUSIC/AiChungTinhDuocMai.mp3',
-        image: '../Image/AiChungTinhDuocMai.jpg'
+        image: '../asset/Image/AiChungTinhDuocMai.jpg'
       },
       {
         name: "Anh Vẫn Ở Đây",
         singer: "Raftaar x kr$na",
         path: '../MUSIC/AnhVanODay.mp3',
-        image: '../Image/AnhVanODay.jpg'
+        image: '../asset/Image/AnhVanODay.jpg'
       },
       {
         name: "Có Hẹn Với Thanh Xuân",
         singer: "Monstar x Freak",
         path: '../MUSIC/CoHenVoiThanhXuan.mp3',
-        image: '../Image/CoHenVoiThanhXuan.jpg'
+        image: '../asset/Image/CoHenVoiThanhXuan.jpg'
       },
       {
         name: "Kẻ Theo Đuổi Ánh Sáng",
         singer: "Huy Vạc",
         path: '../MUSIC/KeTheoDuoiAnhSang.mp3',
-        image: '../Image/KeTheoDuoiAnhSang.jpg' 
+        image: '../asset/Image/KeTheoDuoiAnhSang.jpg' 
       },
       {
         name: "Mãi Chẳng Thuộc Về Nhau",
         singer: "Bozitt",
         path: '../MUSIC/MaiChangThuocVeNhau.mp3',
-        image: '../Image/MaiChangThuocVeNhau.jpg' 
+        image: '../asset/Image/MaiChangThuocVeNhau.jpg' 
       }
 ];
