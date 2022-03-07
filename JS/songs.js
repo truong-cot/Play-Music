3bf15a1773fc6f5f5c60f3e22f962fbb6f97acaa
truong-cot <engtruong1609@gmail.com>
2022-03-07 10:44:48 +0700

    update 2

--- JS/songs.js
+++ JS/songs.js
@@ -2,56 +2,56 @@ export const songs = [
     {
         name: "3107 1",
         singer: "Duongg",
-        path: '../MUSIC/3107 1.mp3',
-        image: '../asset/Image/3107 1.jpg',
+        path: './MUSIC/3107 1.mp3',
+        image: './asset/Image/3107 1.jpg',
       },
       {
         name: "3107 2",
         singer: "Duongg",
-        path: '../MUSIC/3107 2.mp3',
+        path: './MUSIC/3107 2.mp3',
         image:
-        '../asset/Image/3107 2.jpg'
+        './asset/Image/3107 2.jpg'
       },
       {
         name: "3107 3",
         singer: "Duongg",
-        path: '../MUSIC/3107 3.mp3',
-        image: '../asset/Image/3107 3.jpg'
+        path: './MUSIC/3107 3.mp3',
+        image: './asset/Image/3107 3.jpg'
       },
       {
         name: "Đông kiếm em",
         singer: "Vũ ...",
-        path: '../MUSIC/DongKiemEm.mp3',
-        image: '../asset/Image/DongKiemEm.jpg'
+        path: './MUSIC/DongKiemEm.mp3',
+        image: './asset/Image/DongKiemEm.jpg'
       },
       {
         name: "Ai chung tình được mãi",
         singer: "Đinh Tùng Huy",
-        path: '../MUSIC/AiChungTinhDuocMai.mp3',
-        image: '../asset/Image/AiChungTinhDuocMai.jpg'
+        path: './MUSIC/AiChungTinhDuocMai.mp3',
+        image: './asset/Image/AiChungTinhDuocMai.jpg'
       },
       {
         name: "Anh Vẫn Ở Đây",
         singer: "Raftaar x kr$na",
-        path: '../MUSIC/AnhVanODay.mp3',
-        image: '../asset/Image/AnhVanODay.jpg'
+        path: './MUSIC/AnhVanODay.mp3',
+        image: './asset/Image/AnhVanODay.jpg'
       },
       {
         name: "Có Hẹn Với Thanh Xuân",
         singer: "Monstar x Freak",
-        path: '../MUSIC/CoHenVoiThanhXuan.mp3',
-        image: '../asset/Image/CoHenVoiThanhXuan.jpg'
+        path: './MUSIC/CoHenVoiThanhXuan.mp3',
+        image: './asset/Image/CoHenVoiThanhXuan.jpg'
       },
       {
         name: "Kẻ Theo Đuổi Ánh Sáng",
         singer: "Huy Vạc",
-        path: '../MUSIC/KeTheoDuoiAnhSang.mp3',
-        image: '../asset/Image/KeTheoDuoiAnhSang.jpg' 
+        path: './MUSIC/KeTheoDuoiAnhSang.mp3',
+        image: './asset/Image/KeTheoDuoiAnhSang.jpg' 
       },
       {
         name: "Mãi Chẳng Thuộc Về Nhau",
         singer: "Bozitt",
-        path: '../MUSIC/MaiChangThuocVeNhau.mp3',
-        image: '../asset/Image/MaiChangThuocVeNhau.jpg' 
+        path: './MUSIC/MaiChangThuocVeNhau.mp3',
+        image: './asset/Image/MaiChangThuocVeNhau.jpg' 
       }
 ];
